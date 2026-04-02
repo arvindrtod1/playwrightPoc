8c40960ee621b3d87edc114e275da593dd18bfc4
arvindrtod1 <arvindrtod@gmail.com>
2026-04-02 14:05:46 +0530

    Create employee list test cases

---
+++ tests/employees/employeeList.spec.js
@@ -0,0 +1,26 @@
+// employeeList.spec.js
+
+describe('Employee List Tests', () => {
+    beforeEach(() => {
+        // Setup code before each test
+    });
+
+    it('should display the list of employees', () => {
+        // Test case for displaying the employee list
+        expect(true).toBe(true); // Dummy expectation
+    });
+
+    it('should add a new employee', () => {
+        // Test case for adding an employee
+        expect(true).toBe(true); // Dummy expectation
+    });
+
+    it('should delete an employee', () => {
+        // Test case for deleting an employee
+        expect(true).toBe(true); // Dummy expectation
+    });
+
+    afterEach(() => {
+        // Cleanup code after each test
+    });
+});

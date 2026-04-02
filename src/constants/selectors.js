8974d830854a10579565bef51ed2167576d21f57
arvindrtod1 <arvindrtod@gmail.com>
2026-04-02 14:08:33 +0530

    Create src/constants/selectors.js with common selectors and constants

---
+++ src/constants/selectors.js
@@ -0,0 +1,16 @@
+// Common selectors and constants used across pages
+
+const SELECTORS = {
+    BUTTON: 'button',
+    INPUT: 'input',
+    LINK: 'a',
+    // add more common selectors here
+};
+
+const URLS = {
+    HOME: 'https://example.com/',
+    LOGIN: 'https://example.com/login',
+    // add more common URLs here
+};
+
+export { SELECTORS, URLS };

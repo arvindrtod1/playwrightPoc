01d9552c3d11a5415df45690f0bdd56a611e809e
arvindrtod1 <arvindrtod@gmail.com>
2026-04-02 14:05:37 +0530

    Add dashboard test cases

---
+++ tests/dashboard/dashboard.spec.js
@@ -0,0 +1,15 @@
+describe('Dashboard Tests', () => {
+  it('should load the dashboard successfully', () => {
+    // implement test case
+  });
+
+  it('should display the correct user information', () => {
+    // implement test case
+  });
+
+  it('should have the correct number of widgets', () => {
+    // implement test case
+  });
+
+  // more test cases can be added here
+});
